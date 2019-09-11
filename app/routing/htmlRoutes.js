d2e7f55eccb7e22f6a304e8650b4373e7b171358
Stephen <stephen.roy.dsouza@gmail.com>
2019-09-11 20:01:32 +1000

    created links to html routing

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,16 @@
+var path = require("path");
+
+function htmlroutes(app){
+
+    app.get("/survey",function(req,res){
+        res.sendFile(path.join(__dirname,"../public/survey.html"));
+    });
+
+    app.get("/",function(req,res){
+        res.sendFile(path.join(__dirname,"../public/home.html"));
+    });
+
+}
+
+
+module.exports=htmlroutes
