2b9fc9511d70af194cd07a5526406a8bc998c732
Stephen <stephen.roy.dsouza@gmail.com>
2019-09-14 19:17:39 +1000

    added survey questions

---
+++ app/data/friends.js
@@ -0,0 +1,103 @@
+var friends = [
+    {
+      name: "Adam",
+      photo: "https://media.fromthegrapevine.com/assets/images/2015/10/matt-damon-martian-2.jpg.824x0_q71.jpg",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Jill",
+      photo: "https://media.fromthegrapevine.com/assets/images/2015/12/natalie-portman-jackie-kennedy.jpg.824x0_q71.jpg",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "May",
+      photo: "https://ca.hellomagazine.com/celebrities/02017060536318/eco-friendly-celebrities/3",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Louise",
+      photo: "https://www.gettyimages.com.au/detail/photo/smiling-african-woman-in-sunshine-outdoors-profile-royalty-free-image/539293229?adppopup=true&uiloc=thumbnail_similar_images_adp",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "Joey Tribiani",
+      photo: "https://media.giphy.com/media/1AIfV67rM04bflbBDD/giphy-downsized.gif",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Chandler Bing",
+      photo: "http://33.media.tumblr.com/cf9686363fde6ca698a9b5b603eef01a/tumblr_ms0bxt7ThZ1r9zaago1_500.gif",
+      scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    }
+  
+  ];
+  
+
+  module.exports = friends;
+  
